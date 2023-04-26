bd4d2f70345fae71e72bbff7ae7a5e10bf1aa784
Shelley <79370231+shelleytruong@users.noreply.github.com>
2023-04-26 13:51:38 +0100

    Create colours.js

---
+++ colours.js
@@ -0,0 +1,12 @@
+const colors = {
+  black: "#000000",
+  white: "#FFFFFF",
+  red: "#FF0000",
+  green: "#00FF00",
+  blue: "#0000FF",
+  yellow: "#FFFF00",
+  magenta: "#FF00FF",
+  cyan: "#00FFFF"
+};
+
+module.exports = colors;
